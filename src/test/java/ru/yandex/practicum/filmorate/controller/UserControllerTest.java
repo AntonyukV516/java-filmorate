63a4720aedcfbe6297b2e3b804ef26939eabb15d
Viacheslav Antonyuk <antonyukv516@yandex.ru>
2025-02-18 15:29:06 +0300

    CheckStyle

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
@@ -28,7 +27,7 @@ class UserControllerTest {
 
         userController.addUser(new User(0, "email@test.ru", "login", "name",
                 LocalDate.of(2000, 10, 28)));
-        List<User> actualUsers= userController.getUsers();
+        List<User> actualUsers = userController.getUsers();
         assertEquals(expectedUsers, actualUsers);
     }
 
